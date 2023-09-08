3d932c768a2620e838737d8285b2a78c141fca9c
bhuvneshsoni01 <bhuvnesh.soni.ece19@itbhu.ac.in>
2023-09-08 17:13:33 +0530

    Endpoint for fetching the weather data for created!

--- server/server.js
+++ server/server.js
@@ -7,20 +7,27 @@ const request = require("request");
 const App = Express();
 const api_key = '812dafbcf6ece97388b52e72e2ee1aec';
 
-App.get('/', (req,res) => {
+App.get('/', async (req,res) => {
     const city = req.query.city;
-	var request = require('request');
-	request(
-		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`,
-		function(error, response, body, city) {
-			let data = JSON.parse(body);
-			if (response.statusCode === 200) {
-				// res.send(`${data}`)
-				res.send(`The weather in your city "${city}" is ${data.list[0].weather[0].description}`);
-			}
-		}
-	);
+	// request(
+	// 	`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`,
+	// 	function(error, response, body, city) {
+	// 		// let data = JSON.parse(body);
+	// 		if (response.statusCode === 200) {
+	// 			// res.send(`${data}`)
+	// 			console.log(body)
+	// 			res.send(body);
+	// 		}
+	// 	}
+	// );
+	try{
+		const body = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=812dafbcf6ece97388b52e72e2ee1aec`)
+			.then(response => response.json());
+		res.send(body);
+	}catch(err){
+		res.send(err)
+	}
 });
 
 
-App.listen(3000, () => console.log("Server running on port: 3000"))
+App.listen(5000, () => console.log("Server running on port: 5000"))
